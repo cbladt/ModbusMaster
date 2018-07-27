9191e864c82aa4dbb98e3c66af0d21c4cf6a5165
Casper Bladt <cab@lodam.com>
2018-07-28 00:38:22 +0200

    started defining outer interface

--- ModbusMaster/ModbusMaster.cpp
+++ ModbusMaster/ModbusMaster.cpp
@@ -1,6 +1,64 @@
 #include "ModbusMaster.hpp"
 
-ModbusMaster::ModbusMaster()
+namespace ModbusMaster
 {
+    ModbusMaster::ModbusMaster(IDataLink &datalink) :
+        _datalink(datalink)
+    {
+        // ModbusMaster -> Framerouter.
+        Subscribe(_frameRouter);
 
-}
+        // Framerouter -> Strategy.
+        _frameRouter.GetReadInputSubject().Subscribe(_readInputStrategy);
+
+        // Strategy -> Framelayer.
+        _readInputStrategy.Subscribe(_frameLayer);
+
+        // Framelayer -> IDatalink.
+        _frameLayer.Subscribe(_datalink);
+    }
+
+    bool ModbusMaster::ReadParameters(uint8_t slave, const std::vector<Framework::Parameter> &parameters)
+    {
+        if (parameters.empty()) return false;
+
+        // Ensure all params are of the same type.
+        auto type = parameters.front().GetType();
+        for (auto& param : parameters)
+        {
+            if (param.GetType() != type) return false;
+        }
+
+        // Route to the correct method.
+        if (type == Framework::ParameterType::Holding)
+        {
+            return ReadHoldingParameters(slave, parameters);
+        }
+        else if (type == Framework::ParameterType::Input)
+        {
+            return ReadInputParameters(slave, parameters);
+        }
+
+        return false;
+    }
+
+    bool ModbusMaster::WriteParameters(uint8_t slave, const std::vector<Framework::Parameter> &parameters)
+    {
+        if (parameters.empty()) return false;
+
+        RequestModel model(slave, Framework::FunctionCode::WriteMultiple, parameters);
+        return Transmit(model);
+    }
+
+    bool ModbusMaster::ReadHoldingParameters(uint8_t slave, const std::vector<Framework::Parameter> &parameters)
+    {
+        RequestModel model(slave, Framework::FunctionCode::ReadHolding, parameters);
+        return Transmit(model);
+    }
+
+    bool ModbusMaster::ReadInputParameters(uint8_t slave, const std::vector<Framework::Parameter> &parameters)
+    {
+        RequestModel model(slave, Framework::FunctionCode::ReadInput, parameters);
+        return Transmit(model);
+    }
+} // Namespace ModbusMaster.
--- ModbusMaster/ModbusMaster.hpp
+++ ModbusMaster/ModbusMaster.hpp
@@ -1,11 +1,41 @@
 #ifndef MODBUSMASTER_HPP
 #define MODBUSMASTER_HPP
 
+#include <IDataLink.hpp>
+#include <RequestModel.hpp>
+#include <FrameRouter.hpp>
+#include <FrameLayer.hpp>
+#include <Strategies/ReadInputStratety.hpp>
+#include <Framework/TransmitBase.hpp>
+#include <Framework/Parameter.hpp>
 
-class ModbusMaster
+#include <cstdint>
+#include <vector>
+
+namespace ModbusMaster
 {
-public:
-    ModbusMaster();
-};
+    class ModbusMaster :
+            public Framework::TransmitBase<RequestModel>
+    {
+    public:
+        ModbusMaster(IDataLink& datalink);
+
+        bool ReadParameters(uint8_t slave, const std::vector<Framework::Parameter>& parameters);
+
+        bool WriteParameters(uint8_t slave, const std::vector<Framework::Parameter>& parameters);
+
+    private:
+        bool ReadInputParameters(uint8_t slave, const std::vector<Framework::Parameter>& parameters);
+        bool ReadHoldingParameters(uint8_t slave, const std::vector<Framework::Parameter>& parameters);
+
+        IDataLink& _datalink;
+        FrameRouter _frameRouter;
+        FrameLayer _frameLayer;
+
+        Strategies::ReadInputStratety _readInputStrategy;
+    };
+} // Namespace ModbusMaster.
+
+
 
 #endif // MODBUSMASTER_HPP
