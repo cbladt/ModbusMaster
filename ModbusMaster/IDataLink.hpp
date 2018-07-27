37bff1c92a2d0a5d349825fc76c646e480237975
Casper Bladt <cab@lodam.com>
2018-07-28 00:38:03 +0200

    Updated datalink

--- ModbusMaster/IDataLink.hpp
+++ ModbusMaster/IDataLink.hpp
@@ -1,4 +1,22 @@
 #ifndef IDATALINK_HPP
 #define IDATALINK_HPP
 
+#include <Framework/IReceive.hpp>
+
+#include <cstdint>
+#include <vector>
+
+namespace ModbusMaster
+{
+    class IDataLink :
+            public Framework::IReceive<std::vector<uint8_t>>
+
+    {
+    public:
+        virtual ~IDataLink() {}
+
+        bool Receive(std::vector<uint8_t>& bytes) override;
+    };
+} // Namespace ModbusMaster.
+
 #endif // IDATALINK_HPP
