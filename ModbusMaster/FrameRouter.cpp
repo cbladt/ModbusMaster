3d29c4ae234f2300e72c014dbc13917c5bcf18cd
Casper Bladt <cab@lodam.com>
2018-07-28 00:37:55 +0200

    added framerouter

--- ModbusMaster/FrameRouter.cpp
+++ ModbusMaster/FrameRouter.cpp
@@ -1,6 +1,37 @@
 #include "FrameRouter.hpp"
 
-FrameRouter::FrameRouter()
+namespace ModbusMaster
 {
+    FrameRouter::FrameRouter()
+    {}
 
-}
+    bool FrameRouter::Receive(RequestModel &data)
+    {
+        switch (data.GetFunctionCode())
+        {
+            case Framework::FunctionCode::ReadHolding:
+                return _readHoldingSubject.Transmit(data);
+            case Framework::FunctionCode::ReadInput:
+                return _readInputSubject.Transmit(data);
+            case Framework::FunctionCode::WriteMultiple:
+                return _writeMultipleSubject.Transmit(data);
+            default:
+                return false;
+        }
+    }
+
+    Framework::TransmitBase<RequestModel>& FrameRouter::GetReadInputSubject()
+    {
+        return _readInputSubject;
+    }
+
+    Framework::TransmitBase<RequestModel>& FrameRouter::GetReadHoldingSubject()
+    {
+        return _readHoldingSubject;
+    }
+
+    Framework::TransmitBase<RequestModel>& FrameRouter::GetWriteMutipleSubject()
+    {
+        return _writeMultipleSubject;
+    }
+} // Namespace ModbusMaster.
--- ModbusMaster/FrameRouter.hpp
+++ ModbusMaster/FrameRouter.hpp
@@ -1,11 +1,32 @@
 #ifndef FRAMEROUTER_HPP
 #define FRAMEROUTER_HPP
 
+#include <Framework/IReceive.hpp>
+#include <Framework/TransmitBase.hpp>
+#include <Strategies/ReadInputStratety.hpp>
+#include <RequestModel.hpp>
 
-class FrameRouter
+#include <cstdint>
+
+namespace ModbusMaster
 {
-public:
-    FrameRouter();
-};
+    class FrameRouter :
+            public Framework::IReceive<RequestModel>
+    {
+    public:
+        FrameRouter();
+
+        bool Receive(RequestModel& data) override;
+
+        Framework::TransmitBase<RequestModel>& GetReadInputSubject();
+        Framework::TransmitBase<RequestModel>& GetReadHoldingSubject();
+        Framework::TransmitBase<RequestModel>& GetWriteMutipleSubject();
+
+    private:        
+        Framework::TransmitBase<RequestModel> _readInputSubject;
+        Framework::TransmitBase<RequestModel> _readHoldingSubject;
+        Framework::TransmitBase<RequestModel> _writeMultipleSubject;
+    };
+} // Namespace ModbusMaster.
 
 #endif // FRAMEROUTER_HPP
